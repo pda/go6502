0eca0f6a933f5270c290c1367bdd1ab8b2a42732
Paul Annesley <paul@annesley.cc>
2013-07-16 21:59:55 +1000

    12 more instruction types. Runs for 682,705 instructions.
    Eventually crashes on unhandled instruction: TYA.

--- src/c64/cpu.go
+++ src/c64/cpu.go
@@ -106,13 +106,44 @@ func (c *Cpu) memoryAddress(iop *Iop) address {
   switch iop.in.addressing {
   case absolute: return iop.op16
   case absoluteX: return iop.op16 + address(c.x)
+  case absoluteY: return iop.op16 + address(c.y)
+
+  // Indexed indirect addressing.
+  // In indexed indirect addressing (referred to as [Indirect, X]), the second
+  // byte of the instruction is added to the contents of the X index register,
+  // discarding the carry.  The result of this addition points to a memory
+  // location on page zero whose contents is the low order eight bits of the
+  // effective address.  The next memory location in page zero contains the
+  // high order eight bits of the effective address.  Both memory locations
+  // specitying the high and low order bytes of the effective address must be
+  // in page zero.
+  //
+  // TODO: fix this implementation.
+  case indirectX: return c.Bus.Read16(address(iop.op8) + address(c.x))
+
+  // Indirect indexed addressing.
+  // In indirect indexed addressing (referred to as [Indirect, Y]), the second
+  // byte of the instruction points to a memory location in page zero. The
+  // contents of this memory location is added to the contents of the Y index
+  // register, the result being the low oeder eight bits of the effective
+  // address. The carry from this addition is added to the contents of the next
+  // page zero memory location, the result being the high order eight bits of
+  // the effective address.
+  //
+  // TODO: fix this implementation.
+  case indirectY: return c.Bus.Read16(address(iop.op8) + address(c.y))
+
   case zeropage: return address(iop.op8)
   default: panic("unhandled addressing")
   }
 }
 
 func (c *Cpu) getStatus(bit uint8) bool {
-  return c.sr & (1 << bit) > 0
+  return c.getStatusInt(bit) == 1
+}
+
+func (c *Cpu) getStatusInt(bit uint8) uint8 {
+  return (c.sr >> bit) & 1
 }
 
 func (c *Cpu) setStatus(bit uint8, state bool) {
@@ -143,38 +174,72 @@ func (c *Cpu) statusString() string {
   return strings.Join(out[0:], "")
 }
 
+func (c *Cpu) branch(iop *Iop) {
+  relative := int8(iop.op8) // signed
+  if relative >= 0 {
+    c.pc += address(relative)
+  } else {
+    c.pc -= address(-relative)
+  }
+}
+
 func (c *Cpu) Execute(iop *Iop) {
   switch iop.in.name {
+  case "AND": c.AND(iop)
   case "BEQ": c.BEQ(iop)
+  case "BMI": c.BMI(iop)
   case "BNE": c.BNE(iop)
   case "CLD": c.CLD(iop)
   case "CMP": c.CMP(iop)
   case "DEX": c.DEX(iop)
+  case "INC": c.INC(iop)
+  case "INX": c.INX(iop)
+  case "INY": c.INY(iop)
   case "JMP": c.JMP(iop)
   case "JSR": c.JSR(iop)
   case "LDA": c.LDA(iop)
   case "LDX": c.LDX(iop)
+  case "LDY": c.LDY(iop)
+  case "ORA": c.ORA(iop)
+  case "ROL": c.ROL(iop)
   case "RTS": c.RTS(iop)
   case "SEI": c.SEI(iop)
   case "STA": c.STA(iop)
   case "STX": c.STX(iop)
+  case "STY": c.STY(iop)
+  case "TAX": c.TAX(iop)
+  case "TAY": c.TAY(iop)
+  case "TXA": c.TXA(iop)
   case "TXS": c.TXS(iop)
   default: panic(fmt.Sprintf("unhandled instruction: %v", iop.in.name))
   }
+}
+
+// bitwise AND with accumulator
+func (c *Cpu) AND(iop *Iop) {
+  c.ac &= c.resolveOperand(iop)
+  c.updateStatus(c.ac)
 }
 
 // branch on equal (zero set)
 // (branch on z = 1)
 func (c *Cpu) BEQ(iop *Iop) {
   if c.getStatus(sZero) {
-    c.pc += address(iop.op8)
+    c.branch(iop)
+  }
+}
+
+// branch on result minus (status negative)
+func (c *Cpu) BMI(iop *Iop) {
+  if c.getStatus(sNegative) {
+    c.branch(iop)
   }
 }
 
 // branch on not-equal (zero clear)
 func (c *Cpu) BNE(iop *Iop) {
   if !c.getStatus(sZero) {
-    c.pc += address(iop.op8)
+    c.branch(iop)
   }
 }
 
@@ -196,6 +261,32 @@ func (c *Cpu) DEX(iop *Iop) {
   c.updateStatus(c.x)
 }
 
+// decrement y
+func (c *Cpu) DEY(iop *Iop) {
+  c.y--
+  c.updateStatus(c.y)
+}
+
+// increment value in memory
+func (c *Cpu) INC(iop *Iop) {
+  address := c.memoryAddress(iop)
+  value := c.Bus.Read(address) + 1
+  c.Bus.Write(address, value)
+  c.updateStatus(value)
+}
+
+// increment x
+func (c *Cpu) INX(iop *Iop) {
+  c.x++
+  c.updateStatus(c.x)
+}
+
+// increment y
+func (c *Cpu) INY(iop *Iop) {
+  c.y++
+  c.updateStatus(c.y)
+}
+
 // jump
 func (c *Cpu) JMP(iop *Iop) {
   c.pc = c.memoryAddress(iop)
@@ -214,10 +305,35 @@ func (c *Cpu) LDA(iop *Iop) {
   c.updateStatus(c.ac)
 }
 
+// load Y
+func (c *Cpu) LDY(iop *Iop) {
+  c.y = c.resolveOperand(iop)
+  c.updateStatus(c.y)
+}
+
 // load X
 func (c *Cpu) LDX(iop *Iop) {
   c.x = c.resolveOperand(iop)
   c.updateStatus(c.x)
+}
+
+// bitwise OR memory with accumulator
+func (c *Cpu) ORA(iop *Iop) {
+  c.ac |= c.resolveOperand(iop)
+  c.updateStatus(c.ac)
+}
+
+// bitwise rotate left
+// SR carry into bit 0, original bit 7 into SR carry.
+func (c *Cpu) ROL(iop *Iop) {
+  switch iop.in.addressing {
+  case accumulator:
+    carry := c.getStatusInt(sCarry)
+    c.setStatus(sCarry, (c.ac >> 7) == 1)
+    c.ac = (c.ac << 1) | carry
+    c.updateStatus(c.ac)
+  default: panic("ROL non-accumulator addressing not implemented")
+  }
 }
 
 // return from subroutine
@@ -240,6 +356,30 @@ func (c *Cpu) STA(iop *Iop) {
 // store from X
 func (c *Cpu) STX(iop *Iop) {
   c.Bus.Write(c.memoryAddress(iop), c.x)
+}
+
+// store from Y
+func (c *Cpu) STY(iop *Iop) {
+  c.Bus.Write(c.memoryAddress(iop), c.y)
+}
+
+// transfer accumulator for index Y
+func (c *Cpu) TAX(iop *Iop) {
+  c.x = c.ac
+  c.updateStatus(c.x)
+}
+
+// transfer accumulator for index Y
+func (c *Cpu) TAY(iop *Iop) {
+  c.y = c.ac
+  c.updateStatus(c.y)
+}
+
+// Copies the current contents of the X register into the accumulator and sets
+// the zero and negative flags as appropriate.
+func (c *Cpu) TXA(iop *Iop) {
+  c.ac = c.x
+  c.updateStatus(c.ac)
 }
 
 // transfer X to stack pointer
