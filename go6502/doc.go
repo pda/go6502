728488380fe39caa1fcf23683917af872c6cbfac
Paul Annesley <paul@annesley.cc>
2014-05-08 23:05:12 -0700

    go6502 package eliminated; split into new packages.

--- go6502/doc.go
+++
@@ -1,8 +0,0 @@
-/*
-	Package go6502 emulates the pda6502 computer. This includes the MOS 6502
-	processor, memory-mapping address bus, RAM and ROM, MOS 6522 VIA
-	controller, SSD1306 OLED display, and perhaps more.
-
-	Read more at https://github.com/pda/go6502 and https://github.com/pda/pda6502
-*/
-package go6502
