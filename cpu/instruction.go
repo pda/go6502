46f74d9cb3fd647e2d60421f08ab5a72163d2f69
Paul Annesley <paul@annesley.cc>
2014-06-02 23:16:28 -0700

    cpu.ReadInstruction() panics for illegal opcode.

--- cpu/instruction.go
+++ cpu/instruction.go
@@ -34,14 +34,19 @@ func (in Instruction) String() (s string) {
 // ReadInstruction reads an instruction from the bus starting at the given
 // address. An instruction may be 1, 2 or 3 bytes long, including its optional
 // 8 or 16 bit operand.
-func ReadInstruction(PC uint16, bus *bus.Bus) Instruction {
-	in := Instruction{OpType: optypes[bus.Read(PC)]}
+func ReadInstruction(pc uint16, bus *bus.Bus) Instruction {
+	opcode := bus.Read(pc)
+	optype, ok := optypes[opcode]
+	if !ok {
+		panic(fmt.Sprintf("Illegal opcode $%02X at $%04X", opcode, pc))
+	}
+	in := Instruction{OpType: optype}
 	switch in.Bytes {
 	case 1: // no operand
 	case 2:
-		in.Op8 = bus.Read(PC + 1)
+		in.Op8 = bus.Read(pc + 1)
 	case 3:
-		in.Op16 = bus.Read16(PC + 1)
+		in.Op16 = bus.Read16(pc + 1)
 	default:
 		panic(fmt.Sprintf("unhandled instruction length: %d", in.Bytes))
 	}
